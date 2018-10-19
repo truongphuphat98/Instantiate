18f2a5d2d43f0626c995ebbcffceec0eeaea5e0a
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-19 17:50:00 +0200

    Add Instantiate Project

---
+++ SmoothSphereFountain/Assets/SphereFountain/Scripts/InstantiatePrefab.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiatePrefab : MonoBehaviour
+{
+    public GameObject prefab;
+
+    [Header("Prefab Life")]
+    float spamLife = .5f;
+    int normalLife = 5;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Destroy(Instantiate(prefab.gameObject, new Vector3(), Quaternion.identity), spamLife);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Destroy(Instantiate(prefab.gameObject, new Vector3(), Quaternion.identity), normalLife);
+        }
+
+    }
+}
